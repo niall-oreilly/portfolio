3ff49c0d24b62255214a3ba2f1ce4b7d2929dc9d
Niall OReilly <nialloreilly123@gmail.com>
2016-12-29 02:36:16 +0000

    folder structure nearly done

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,21 @@
-var tabs = $('.tabs > li');
+$(document).ready(function(){
+	var tabs = $('.tabs > li');
 
-tabs.on("click", function(){
-  tabs.removeClass('active');
-  $(this).addClass('active');
+	tabs.on("click", function(){
+	  tabs.removeClass('active');
+	  $(this).addClass('active');
+	});
+
+	var files = $('.file');
+
+	files.click(function(){
+		files.removeClass('active');
+		$(this).addClass('active');
+	})
+
+	$('.expandable').click(function(){
+		$(this).toggleClass('open');
+		$(this).next('ul').toggleClass('open');
+	});
+
 });
