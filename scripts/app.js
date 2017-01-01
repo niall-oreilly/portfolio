66105fac3530c17196a00c4165f39e45fc5f5850
Niall OReilly <nialloreilly123@gmail.com>
2017-01-01 18:54:22 +0000

    added proper file names

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var tabs = $('.tabs > li');
 
 	tabs.on("click", function(){
@@ -16,6 +17,22 @@ $(document).ready(function(){
 	$('.expandable').click(function(){
 		$(this).toggleClass('open');
 		$(this).next('ul').toggleClass('open');
+	});
+
+function clickableLinks() {
+	$('code a').attr('contenteditable', false);
+}
+
+	$.ajax({
+		url: 'content/test.html',
+		dataType: 'text',
+		success: function(response){
+			$('code').html(response);
+			Prism.highlightElement($('code')[0], false, clickableLinks);
+		},
+		error: function(xhr, status, error){
+			console.log('XHR error:', xhr, status, error);
+		} 
 	});
 
 });
