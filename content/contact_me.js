02f1cb44440beddc097d4306f242760727033f91
Niall OReilly <nialloreilly123@gmail.com>
2019-03-19 13:13:06 +1100

    updating info for aus

--- content/contact_me.js
+++ content/contact_me.js
@@ -1,13 +1,14 @@
 
 // Get in touch! I'd love to hear from you.
 
-exports.getPhoneNumber = () => '086 075 3519';
+exports.getPhoneNumber = () => '0497506802';
 
 exports.getEmailAddress = () => 'nialloreilly123@gmail.com';
 
-exports.getLocation = () => `Calderwood Avenue,
-							Drumcondra,
-							Dublin 9`;
+exports.getLocation = () => `	16 Llandaff St,
+								Bondi Junction,
+								Sydney,
+								New South Wales`;
 
 exports.getLinkedIn = () => 'https://www.linkedin.com/in/nialloreillydev/';
 
